c63812be66cd58510956a56eb8bf0ef4c2524dd2
Rabindra <pangenirabindra5@gmail.com>
2020-04-06 11:05:29 +0545

    made separate Layout File for separate page.

---
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesCareersPageLayoutVue.groovy
@@ -0,0 +1,22 @@
+package spring.globech.elintegroWebsite
+
+import com.elintegro.erf.dataframe.Dataframe
+import com.elintegro.erf.layout.ColumnLayoutVue
+import com.elintegro.erf.layout.RowLayoutVue
+import grails.util.Holders
+
+beans {
+    vueCareersDataframeLayout(ColumnLayoutVue){ bean->
+        layoutBeanName = bean.name
+        layoutPlaceHolder = """<v-flex xs12 sm12 md12 lg12 xl12><v-card><v-toolbar dark color="grey darken-3" height="100px" style="margin-bottom:30px;">
+                                 <v-toolbar-title class="white--text" style="margin:100px;">[DATAFRAME_LABEL]</v-toolbar-title>
+                                 <v-spacer></v-spacer>
+                                 <vueCareersPageButtonDataframe/>
+                                  </v-toolbar>[DATAFRAME_SCRIPT][BUTTON_SCRIPT]</v-card></v-flex>"""
+        flexGridValues = ['xs12', 'sm6', 'md4', 'lg4', 'xl4']
+
+    }
+
+
+
+}
---
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesNewEmployeeApplicantPageLayoutVue.groovy
@@ -0,0 +1,31 @@
+package spring.globech.elintegroWebsite
+
+import com.elintegro.erf.dataframe.Dataframe
+import com.elintegro.erf.layout.ColumnLayoutVue
+import com.elintegro.erf.layout.RowLayoutVue
+import grails.util.Holders
+
+beans {
+    vueNewEmployeeApplicantDataframeLayout(ColumnLayoutVue){bean->
+        layoutBeanName = bean.name
+        layoutPlaceHolder = """<v-flex xs12 sm12 md12 lg12 xl12><v-card round >
+                                <v-tabs color="light-blue darken-2" dark slider-color="yellow" v-model="vueNewEmployeeApplicantDataframe_tab_model">
+                                    <v-tab style ="text-transform:capitalize;" ripple href="#vueNewEmployeeBasicInformationDataframe-tab-id">Basic Information</v-tab>
+                                    <v-tab style ="text-transform:capitalize;" ripple href="#vueNewEmployeeUploadResumeDataframe-tab-id">Upload Resume</v-tab>
+                                     <v-tab style ="text-transform:capitalize;" ripple href="#vueNewEmployeeSelfAssesmentDataframe-tab-id">Self Assesment</v-tab>
+                                   <v-tab style ="text-transform:capitalize;" ripple href="#vueNewEmployeeAddtionalQuestionsDataframe-tab-id">Additional Questions</v-tab>
+                                 </v-tabs>
+                                 
+                                  <v-tabs-items v-model="vueNewEmployeeApplicantDataframe_tab_model">
+                                    <v-tab-item value="vueNewEmployeeBasicInformationDataframe-tab-id">
+                                     <vueNewEmployeeBasicInformationDataframe/>
+                                     </v-tab-item>
+                                     <v-tab-item value="vueNewEmployeeUploadResumeDataframe-tab-id"><vueNewEmployeeUploadResumeDataframe/></v-tab-item>
+                                     <v-tab-item value="vueNewEmployeeSelfAssesmentDataframe-tab-id"><vueNewEmployeeSelfAssesmentDataframe/></v-tab-item>
+                                     <v-tab-item value="vueNewEmployeeAddtionalQuestionsDataframe-tab-id"><vueNewEmployeeAddtionalQuestionsDataframe/></v-tab-item>
+                                     </v-tabs-items></v-card></v-flex>
+                                 </v-flex>"""
+    }
+
+
+}
