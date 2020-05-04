439ae9da78e6beffb531427e5d47dda3e22efd69
Rabindra <pangenirabindra5@gmail.com>
2020-05-04 08:55:26 +0545

    refactor layout things

--- elintegrostartapp/grails-app/conf/spring/globech/resourcesScriptBean.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/resourcesScriptBean.groovy
@@ -20,7 +20,7 @@ beans {
 
     def contextPath = Holders.grailsApplication.config.rootPath
     vueInitDataframe_script(VueJsEntity) { bean ->
-        created = """this.setInitPageValues();"""
+        created = """this.setInitPageValues();this.setupHomePage();"""
 
         methods =
                 """  setupHomePage: function(){
