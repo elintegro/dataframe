7c0a7e492fb5136f95d2636ef79dcb36d498bec8
Rabindra <pangenirabindra5@gmail.com>
2020-05-28 11:08:11 +0545

    fix some UI things

--- elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesCareersPageLayoutVue.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesCareersPageLayoutVue.groovy
@@ -9,7 +9,7 @@ beans {
     vueCareersDataframeLayout(ColumnLayoutVue){ bean->
         layoutBeanName = bean.name
         layoutPlaceHolder = """<v-flex xs12 sm12 md12 lg12 xl12><v-card><v-toolbar dark color="grey darken-3" height="100px" style="margin-bottom:30px;">
-                                 <v-toolbar-title class="white--text" style="margin:100px;">[DATAFRAME_LABEL]</v-toolbar-title>
+                                 <v-toolbar-title class="white--text">[DATAFRAME_LABEL]</v-toolbar-title>
                                  <v-spacer></v-spacer>
                                  <vueCareersPageButtonDataframe/>
                                   </v-toolbar>[DATAFRAME_SCRIPT][BUTTON_SCRIPT]</v-card></v-flex>"""
