40c51eb16520aec4ecc6bc7197d1595718a697c6
Rabindra <pangenirabindra5@gmail.com>
2020-07-08 16:04:30 +0545

    displayed profile picture(in Profile and EditProfile page)...

--- elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
+++ elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
@@ -4,10 +4,12 @@ import com.elintegro.crm.Person
 import com.elintegro.elintegrostartapp.hr.Position
 import com.elintegro.ref.Language
 import grails.converters.JSON
+import grails.util.Holders
 
 import java.text.SimpleDateFormat
 
 class ProfileDetailController {
+    def springSecurityService
 
     def editProfileData() {
         def profileData = request.getJSON()
@@ -21,11 +23,11 @@ class ProfileDetailController {
         person.bday = date
         person.mainPicture = profileData.vueElintegroUserProfileDataframe_propertyImages
         person.phone = profileData.vueElintegroUserProfileDataframe_person_phone
-//            for (item in profileData.vueElintegroUserProfileDataframe_person_languages) {
-//                Language languages = Language.findById(item.id)
-//                person.addToLanguages(languages)
+            for (item in profileData.vueElintegroUserProfileDataframe_person_languages) {
+                Language languages = Language.findById(item.id)
+                person.addToLanguages(languages)
              person.save(flush:true)
-//            }
+            }
         def profileDetail = [person: profileData]
          resultData = [success: true,params:profileData,newData: profileDetail]
     }
@@ -33,5 +35,27 @@ class ProfileDetailController {
             log.error("Failed to edit profile details "+ e)
         }
         render(resultData as JSON)
+    }
+    def imageData = {
+        def currentUser = springSecurityService.currentUser
+        Person person = Person.findById(currentUser.id)
+        def imageName
+        def imageUrl
+        if(person.mainPicture != null){
+            imageName = person.mainPicture
+            imageUrl = Holders.grailsApplication.config.images.storageLocation + "/images/" + imageName
+
+        }
+        else{
+            imageUrl = 'C:/dev/github/elintegro/dataframe/elintegrostartapp/grails-app/assets/images/default_profile.jpg'
+            imageName = Holders.grailsApplication.config.images.defaultImageName
+        }
+        def file = new File(imageUrl)
+        def extension = imageName - ~/.*(?<=\.)/
+        response.setContentType("application/"+extension)
+        response.setContentLength(file.size().toInteger())
+        OutputStream out = response.getOutputStream();
+        out.write(file.bytes);
+        out.close();
     }
 }
