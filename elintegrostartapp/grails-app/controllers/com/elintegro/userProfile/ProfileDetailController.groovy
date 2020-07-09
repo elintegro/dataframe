afbb44fc9e9d5aa3f29d616cb441d4477b4e3326
Rabindra <pangenirabindra5@gmail.com>
2020-07-09 12:51:00 +0545

    displayed profile picture(in Profile and EditProfile page)...

--- elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
+++ elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
@@ -41,29 +41,35 @@ class ProfileDetailController {
         Person person = Person.findById(currentUser.id)
         def imageName
         def imageUrl
-        if(person.mainPicture != null){
+        if (person.mainPicture != null) {
             imageName = person.mainPicture
             imageUrl = Holders.grailsApplication.config.images.storageLocation + "/images/" + imageName
 
-        }
-        else{
-            def defaultUrl = Holders.grailsApplication.config.grails.serverURL
+        } else {
+            def serverUrl = Holders.grailsApplication.config.grails.serverURL
             imageName = Holders.grailsApplication.config.images.defaultImageName
-            if(defaultUrl == "http://localhost:8099"){
+            if (serverUrl == "http://localhost:8099") {
                 imageUrl = '/dev/github/elintegro/dataframe/elintegrostartapp/grails-app/assets/images/default_profile.jpg'
+            } else {
+                imageUrl = Holders.grailsApplication.config.images.storageLocation + "/ROOT/assets/default_profile.jpg"
             }
-            else {
-                imageUrl = Holders.grailsApplication.config.images.storageLocation + "ROOT/assets/default_profile.jpg"
-            }
-
 
         }
         def file = new File(imageUrl)
-        def extension = imageName - ~/.*(?<=\.)/
-        response.setContentType("application/"+extension)
-        response.setContentLength(file.size().toInteger())
-        OutputStream out = response.getOutputStream();
-        out.write(file.bytes);
-        out.close();
+        if (file.exists()) {
+            try {
+                def extension = imageName - ~/.*(?<=\.)/
+                response.setContentType("application/" + extension)
+                response.setContentLength(file.size().toInteger())
+                OutputStream out = response.getOutputStream();
+                out.write(file.bytes);
+                out.close();
+            }catch(Exception e){
+                log.error("Image couldn't be generated"+e)
+            }
+        }
+        else{
+            log.error("Image doesn't exist")
+        }
     }
 }
