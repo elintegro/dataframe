15380ebaf01e1c13f5e9c345eebf6aa87cd53012
Rabindra <pangenirabindra5@gmail.com>
2020-07-09 12:43:50 +0545

    displayed profile picture(in Profile and EditProfile page)...

--- elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
+++ elintegrostartapp/grails-app/controllers/com/elintegro/userProfile/ProfileDetailController.groovy
@@ -14,25 +14,25 @@ class ProfileDetailController {
     def editProfileData() {
         def profileData = request.getJSON()
         def resultData
-        try{
-        Person person = Person.findById(profileData.vueElintegroUserProfileDataframe_person_id)
-        person.firstName = profileData.vueElintegroUserProfileDataframe_person_firstName
-        person.lastName = profileData.vueElintegroUserProfileDataframe_person_lastName
+        try {
+            Person person = Person.findById(profileData.vueElintegroUserProfileDataframe_person_id)
+            person.firstName = profileData.vueElintegroUserProfileDataframe_person_firstName
+            person.lastName = profileData.vueElintegroUserProfileDataframe_person_lastName
             SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
             Date date = inputFormat.parse(profileData.vueElintegroUserProfileDataframe_person_bday)
-        person.bday = date
-        person.mainPicture = profileData.vueElintegroUserProfileDataframe_propertyImages
-        person.phone = profileData.vueElintegroUserProfileDataframe_person_phone
+            person.bday = date
+            person.mainPicture = profileData.vueElintegroUserProfileDataframe_propertyImages
+            person.phone = profileData.vueElintegroUserProfileDataframe_person_phone
             for (item in profileData.vueElintegroUserProfileDataframe_person_languages) {
                 Language languages = Language.findById(item.id)
                 person.addToLanguages(languages)
-             person.save(flush:true)
+                person.save(flush: true)
             }
-        def profileDetail = [person: profileData]
-         resultData = [success: true,params:profileData,newData: profileDetail]
-    }
-        catch (Exception e){
-            log.error("Failed to edit profile details "+ e)
+            def profileDetail = [person: profileData]
+            resultData = [success: true, params: profileData, newData: profileDetail]
+        }
+        catch (Exception e) {
+            log.error("Failed to edit profile details " + e)
         }
         render(resultData as JSON)
     }
@@ -47,8 +47,16 @@ class ProfileDetailController {
 
         }
         else{
-            imageUrl = 'C:/dev/github/elintegro/dataframe/elintegrostartapp/grails-app/assets/images/default_profile.jpg'
+            def defaultUrl = Holders.grailsApplication.config.grails.serverURL
             imageName = Holders.grailsApplication.config.images.defaultImageName
+            if(defaultUrl == "http://localhost:8099"){
+                imageUrl = '/dev/github/elintegro/dataframe/elintegrostartapp/grails-app/assets/images/default_profile.jpg'
+            }
+            else {
+                imageUrl = Holders.grailsApplication.config.images.storageLocation + "ROOT/assets/default_profile.jpg"
+            }
+
+
         }
         def file = new File(imageUrl)
         def extension = imageName - ~/.*(?<=\.)/
