75fa30c87eaed5bd39fb846d6c5690bf0bf2d55e
Rabindra <pangenirabindra5@gmail.com>
2020-07-10 15:58:50 +0545

    tried to display picture in applicant CV tab

--- elintegrostartapp/grails-app/controllers/com/elintegro/FileDownloadController.groovy
+++ elintegrostartapp/grails-app/controllers/com/elintegro/FileDownloadController.groovy
@@ -1,5 +1,7 @@
 package com.elintegro
 
+import com.elintegro.elintegrostartapp.client.Application
+import com.elintegro.elintegrostartapp.hr.Images
 import grails.util.Holders
 
 class FileDownloadController {
@@ -44,6 +46,31 @@ class FileDownloadController {
         }
         else {
             log.error("Such file +$fileName+ doesn't exist.")
+        }
+    }
+
+    def imagePreview(){
+        def params = request.getJSON()
+        def applicantId = params.id
+        Set<Images> images = Application.findById(applicantId).images
+        println(images)
+        def imageName = images[0].name
+        def imageUrl = Holders.grailsApplication.config.images.storageLocation + "/images/" + imageName
+        def imageFile = new File(imageUrl)
+        if (imageFile.exists()) {
+            try {
+                def extension = imageName - ~/.*(?<=\.)/
+                response.setContentType("application/" + extension)
+                response.setContentLength(imageFile.size().toInteger())
+                OutputStream out = response.getOutputStream();
+                out.write(imageFile.bytes);
+                out.close();
+            }catch(Exception e){
+                log.error("Image couldn't be generated"+e)
+            }
+        }
+        else{
+            log.error("Image doesn't exist")
         }
     }
 }
