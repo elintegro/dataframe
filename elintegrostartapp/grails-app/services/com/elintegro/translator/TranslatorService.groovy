54ba7d1ee0948f4fb00478deb9b1790bf5bb396c
simon Norris <sumanneupane19982055@gmail.com>
2020-07-22 12:10:54 +0545

    validated the .properties file

--- elintegrostartapp/grails-app/services/com/elintegro/translator/TranslatorService.groovy
+++ elintegrostartapp/grails-app/services/com/elintegro/translator/TranslatorService.groovy
@@ -7,25 +7,35 @@ class TranslatorService {
     def loadTexts(String fileName, String language, String projectName){
         def file = Holders.grailsApplication.config.images.storageLocation+"/images/"+fileName
         File sourceFile = new File(file);
-        BufferedReader br = new BufferedReader(new FileReader(sourceFile));
-        String line = null;
-        while ( (line = br.readLine()) != null){
-            String[] keyValue = line.split("=");
-            if(keyValue.length == 2){
-                String key = keyValue[0];
-                String sourceText = keyValue[1];
-                Project project = Project.findByName(projectName)
-                Text text = new Text()
-                text._key = key
-                text.text = sourceText
-                text.project = project
-                text.language = language
-                text.save(flush:true)
+        def lines = sourceFile.readLines()
+        def targetLabels = []
+        lines.each { record ->
+            def a =validateTextRecord( record,fileName,language,projectName)
+            if(a==true) {
+                String[] keyValue = record.split("=")
+                if (keyValue.length == 2) {
+                    String key = keyValue[0]
+                    String sourceText = keyValue[1]
+                    Project project = Project.findByName(projectName)
+                    Text text = new Text()
+                    text._key = key
+                    text.text = sourceText
+                    text.project = project
+                    text.language = language
+                    text.save(flush: true)
+                }
             }
         }
-
-
     }
+    boolean validateTextRecord(String record,String fileName, String language, String projectName) {
+        if (record?.indexOf("=") > 0){
+            return true
+        }
+        log.warn("Translating project ${projectName} from ${language} , while loading file ${fileName}: the following line is corrupted: ${record} . The record will not be inserted into the database!")
+        return false
+    }
+
+
 
 
 }
