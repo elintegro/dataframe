6d717f84957c4740e809048d6b8cb3420b9b81da
Rabindra <pangenirabindra5@gmail.com>
2020-07-30 15:48:54 +0545

    sent elintegro logged in user's data to quizzable app with post request..

--- elintegrostartapp/grails-app/controllers/com/elintegro/quizzable_elintegro/QuizzableLoginController.groovy
+++ elintegrostartapp/grails-app/controllers/com/elintegro/quizzable_elintegro/QuizzableLoginController.groovy
@@ -1,6 +1,10 @@
 package com.elintegro.quizzable_elintegro
 
+import grails.converters.JSON
+import grails.util.Environment
 import grails.util.Holders
+
+import java.nio.charset.Charset
 
 class QuizzableLoginController {
     def springSecurityService
@@ -12,8 +16,49 @@ class QuizzableLoginController {
         def lastName = currentUser.lastName
         def email = currentUser.email
         def param = request.getJSON()
-        def quizzableUrl = Holders.grailsApplication.config.quizzableUrl
-        println(param)
+        def quizzableUrl
+        if(Environment.current == Environment.DEVELOPMENT) {
+            quizzableUrl = "http://localhost:8090/login/userDetailsFromElintegro"
+        }
+        else
+        {
+            quizzableUrl = Holders.grailsApplication.config.quizzableUrl+"login/userDetailsFromElintegro"
+        }
+//        GET REQUEST:
+//        try {
+//            String purl = "http://localhost:8090/login/userDetailsFromElintegro/$userName"
+//            def url = new URL(purl)
+//            def conn = url.openConnection()
+//            conn.doOutput = true
+//            def writer = new OutputStreamWriter(conn.getOutputStream())
+//            writer.flush()
+//            String out = conn.inputStream.text
+//        }catch(e){
+//              log.error("Some error"+e)
+//        }
 
+//      POST REQUEST:
+        String urlParameters  = "userName=${userName}&firstName=${firstName}&lastName=${lastName}&email=${email}";
+        byte[] postData       = urlParameters.getBytes(Charset.forName("UTF-8"))
+        int    postDataLength = postData.length;
+        String request        = quizzableUrl;
+        URL    url            = new URL( request );
+        HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+        conn.setDoOutput( true );
+        conn.setInstanceFollowRedirects( false );
+        conn.setRequestMethod( "POST" );
+        conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty( "charset", "utf-8");
+        conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+        conn.setUseCaches( false );
+        conn.connect()
+        try{
+            DataOutputStream wr = new DataOutputStream( conn.getOutputStream())
+            wr.write( postData );
+            String out = conn.inputStream.text
+            //Integer out = conn.getResponseCode()//(You can do this instead of above line)
+        }catch(e){
+            log.error("Cann't send request to the quizzable."+e)
+        }
     }
 }
