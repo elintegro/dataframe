1e4affda16693d8d17f6a55b6a7fa9b22f735d19
simon Norris <sumanneupane19982055@gmail.com>
2020-08-12 15:43:17 +0545

    successfully validated that the key field should be English and also performed unit test for regular expressions

--- elintegrostartapp/grails-app/services/com/elintegro/translator/FileValidationService.groovy
+++ elintegrostartapp/grails-app/services/com/elintegro/translator/FileValidationService.groovy
@@ -10,20 +10,28 @@ class FileValidationService{
         def file = Holders.grailsApplication.config.images.storageLocation + "/images/" + "${projectName}" + "/" + fileName
         File sourceFile = new File(file)
         def lines = sourceFile.readLines()
-            lines.each { record ->
-                String[] keyValue = record.split("=")
-                if (keyValue.length == 2) {
-                    String key = keyValue[0]
-                   // Pattern regExPattern = Pattern.compile(/([A-Z]|[a-z]|[0-9]|(\.|\-|\_)|\s)+/, Pattern.CASE_INSENSITIVE);
-                    Pattern regExPattern = Pattern.compile(/[a-zA-Z0-9.-_]+/, Pattern.CASE_INSENSITIVE);
-                    Matcher matcher = regExPattern.matcher(key);
-                    if (!matcher.find()) { //Not English!
-                        return false
-                    }else{
-                        return true
+        def count = 0
+        lines.each { record ->
+            if(count==0) {
+                    String[] keyValue = record.split("=")
+                    if (keyValue.length == 2) {
+                        String key = keyValue[0]
+                        // Pattern regExPattern = Pattern.compile(/([A-Z]|[a-z]|[0-9]|(\.|\-|\_)|\s)+/, Pattern.CASE_INSENSITIVE);
+                        Pattern regExPattern = Pattern.compile(/^[a-zA-Z0-9\u0024@\u0024!%*?&#^-_. +]+\u0024/, Pattern.CASE_INSENSITIVE);
+                        Matcher matcher = regExPattern.matcher(key);
+                        if (!matcher.find()) { //Not English!
+                            return count = 1
+                        } else {
+                            return true
+                        }
                     }
                 }
             }
+        if(count==1){
+            return false
+        }else{
+            return true
+        }
 
     }
 }
