2b00a4909650a381007b19a719a0eb4ee871d50f
simon Norris <sumanneupane19982055@gmail.com>
2020-10-05 15:49:11 +0545

    fixing the home page design

--- elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroHomeLayoutVue.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroHomeLayoutVue.groovy
@@ -191,47 +191,47 @@ beans{
                                                    <h2 class="heading-2">[thisIsHow]</h2></v-col>
                                                 </v-row><br><br>
                                                 <v-row>
-                                                    <v-col cols="1"></v-col>
-                                                    <v-col cols="3" class="images">    
+                                                    <v-col cols="2"></v-col>
+                                                    <v-col cols="2" xs="3" sm="3" md="2" xl="2" lg="2" class="images">    
                                                         <v-img src="assets/home/java.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col>
                                                         <v-col cols="2"  class="images">
-                                                        <v-img src="assets/home/javascript.png"></v-img>
+                                                        <v-img src="assets/home/nodejs.png"></v-img>
                                                     </v-col> 
                                                     <v-col cols="1"></v-col> 
-                                                        <v-col cols="3"  class="images">
-                                                        <v-img src="assets/home/grails.png"></v-img>
+                                                        <v-col cols="2"  class="images">
+                                                        <v-img src="assets/home/mysql.png"></v-img>
                                                     </v-col>
-                                                    <v-col cols="1"></v-col> 
+                                                    <v-col cols="2"></v-col> 
                                                 </v-row>
-                                                 <v-row>
-                                                    <v-col cols="1"></v-col>
-                                                        <v-col cols="2" class="images">
+                                                <v-row>
+                                                    <v-col cols="2"></v-col>
+                                                        <v-col cols="1" class="images">
                                                         <v-img src="assets/home/vue.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col>
                                                         <v-col cols="3" class="images">
-                                                        <v-img src="assets/home/kafka.png"></v-img>
+                                                        <v-img src="assets/home/grails.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col> 
-                                                        <v-col cols="3"  class="images">
-                                                        <v-img src="assets/home/oracle.png"></v-img>
+                                                        <v-col cols="2"  class="images">
+                                                        <v-img src="assets/home/kubernetes.png"></v-img>
                                                     </v-col>
-                                                    <v-col cols="1"></v-col>
+                                                    <v-col cols="2"></v-col>
                                                 </v-row>
                                                 <v-row>
-                                                    <v-col cols="1"></v-col> 
-                                                    <v-col cols="3" class="images">
-                                                        <v-img src="assets/home/nodejs.png"></v-img>
+                                                    <v-col cols="2"></v-col> 
+                                                    <v-col cols="2" class="images">
+                                                        <v-img src="assets/home/javascript.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col> 
                                                         <v-col cols="2" class="images">
-                                                        <v-img src="assets/home/kubernetes.png"></v-img>
+                                                        <v-img src="assets/home/oracle.png"></v-img>
                                                     </v-col> 
                                                     <v-col cols="1"></v-col> 
                                                         <v-col cols="3" class="images">
-                                                        <v-img src="assets/home/mysql.png"></v-img>
+                                                        <v-img src="assets/home/kafka.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col> 
                                                 </v-row>
