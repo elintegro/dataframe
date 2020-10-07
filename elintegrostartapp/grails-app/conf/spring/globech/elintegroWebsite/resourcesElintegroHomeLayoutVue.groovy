cd3f577d1ab9cfe479698b9960416d204b299260
simon Norris <sumanneupane19982055@gmail.com>
2020-10-07 10:21:35 +0545

    fixed the page to be in french

--- elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroHomeLayoutVue.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroHomeLayoutVue.groovy
@@ -37,7 +37,7 @@ beans{
                                              <v-col cols="12" xs="10" sm="10" md="4" xl="4" lg="4" align-self = 'center'>
                                                 <v-row>
                                                     <h2 class="heading-2">[build]</h2>[buildData]
-                                                </v-row><br>
+                                                </v-row>
                                                 <v-row>
                                                    <p class="paragraph">[youWont]
                                                    </p>
@@ -210,7 +210,7 @@ beans{
                                                 </v-row>
                                                 <v-row>
                                                     <v-col cols="2"></v-col>
-                                                        <v-col cols="1" class="images">
+                                                        <v-col cols="2" class="images">
                                                         <v-img src="assets/home/vue.png"></v-img>
                                                     </v-col>
                                                     <v-col cols="1"></v-col>
@@ -221,14 +221,14 @@ beans{
                                                         <v-col cols="2"  class="images">
                                                         <v-img src="assets/home/kubernetes.png"></v-img>
                                                     </v-col>
-                                                    <v-col cols="2"></v-col>
+                                                    <v-col cols="1"></v-col>
                                                 </v-row>
                                                 <v-row>
                                                     <v-col cols="2"></v-col> 
-                                                    <v-col cols="1" class="images">
+                                                    <v-col cols="2" class="images">
                                                         <v-img src="assets/home/javascript.png"></v-img>
                                                     </v-col>
-                                                    <v-col cols="2"></v-col> 
+                                                    <v-col cols="1"></v-col> 
                                                         <v-col cols="2" class="images">
                                                         <v-img src="assets/home/oracle.png"></v-img>
                                                     </v-col> 
