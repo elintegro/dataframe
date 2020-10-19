db42a18b5b4c0880758c8a3d8bca15ae0d4eed09
Rabindra5 <58142557+Rabindra5@users.noreply.github.com>
2020-10-19 19:54:27 +0545

    Qa (#206)

---
+++ elintegrostartapp/src/test/groovy/com/elintegro/elintegrostartapp/client/Lead/GroovySpec.groovy
@@ -0,0 +1,18 @@
+package com.elintegro.elintegrostartapp.client.Lead
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class GroovySpec extends Specification implements DomainUnitTest<Groovy> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ elintegrostartapp/src/test/groovy/com/elintegro/register/LoginWithGoogleControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.elintegro.register
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class LoginWithGoogleControllerSpec extends Specification implements ControllerUnitTest<LoginWithGoogleController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
