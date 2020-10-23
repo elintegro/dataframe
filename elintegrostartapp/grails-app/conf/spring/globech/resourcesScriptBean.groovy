f34b53422095a2a276628042e2cf335126e2535c
Simon Neupane <42701109+Simon-Norris@users.noreply.github.com>
2020-10-23 16:21:10 +0545

    animation of word in middle (#213)

--- elintegrostartapp/grails-app/conf/spring/globech/resourcesScriptBean.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/resourcesScriptBean.groovy
@@ -90,50 +90,34 @@ beans {
                     changeWords(){
                             var text = document.getElementById("buildData").innerHTML;
                             var words = text.split(',');
-                            var colors = ['royalblue','mediumseagreen','aqua','gold','crimson']
-                            var visible = true;
                             var con = document.getElementById('console');
                             var letterCount = 1;
                             var x = 1;
                             var waiting = false;
                             var target = document.getElementById("build");
-                            target.setAttribute('style', 'color:' + colors[0])
                             window.setInterval(function() {
                                 if (letterCount === 2 && waiting === false) {
                                   waiting = true;
                                   target.innerHTML = words[0].substring(0, letterCount)
                                   window.setTimeout(function() {
-                                        var usedColor = colors.shift();
-                                        colors.push(usedColor);
                                         var usedWord = words.shift();
                                         words.push(usedWord);
                                         x = 1;
-                                        target.setAttribute('style', 'color:' + colors[0])
                                         letterCount += x;
                                         waiting = false;
-                                  }, 3000)
+                                  }, 1000)
                                 }else if (letterCount === words[0].length + 1 && waiting === false) {
                                       waiting = true;
                                       window.setTimeout(function() {
                                         x = -1;
                                         letterCount += x;
                                         waiting = false;
-                                      }, 3000)
+                                      }, 1000)
                                 }else if (waiting === false) {
                                       target.innerHTML = words[0].substring(0, letterCount)
                                       letterCount += x;
                                       }
-                            }, 220)
-                            window.setInterval(function() {
-                                if (visible === true) {
-                                  con.className = 'console-underscore hidden'
-                                  visible = false;
-                                } else {
-                                  con.className = 'console-underscore'
-                                  visible = true;
-                                }
-                            }, 600)
-
+                            }, 120)
                     },\n           
 
                     """
