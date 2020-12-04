8fb3e2cb21c593e87b7e23ac1a7b79a40e3d387a
Rabindra <pangenirabindra5@gmail.com>
2020-12-04 11:47:23 +0545

    working on contact us job issue...

---
+++ elintegrostartapp/grails-app/jobs/com/elintegro/website/ContactUsMessageJob.groovy
@@ -0,0 +1,18 @@
+package com.elintegro.website
+
+import grails.util.Holders
+
+class ContactUsMessageJob {
+    def contactUsMessageService
+    static triggers = {
+        def conf = Holders.grailsApplication.config
+        Long emailInterval = conf.elintegro.contuctus.email.interval
+        simple repeatInterval: emailInterval //send email once in 20 seconds is a configuration
+    }
+
+    def execute() {
+        //  println("Job Run")
+        contactUsMessageService.sendingMessages()
+
+    }
+}
