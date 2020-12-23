e050087902f1ed4298dfdcbead1d9b1a306b3314
Rabindra5 <58142557+Rabindra5@users.noreply.github.com>
2020-12-23 13:07:02 +0545

    Development (#262)

--- elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroVue.groovy
+++ elintegrostartapp/grails-app/conf/spring/globech/elintegroWebsite/resourcesElintegroVue.groovy
@@ -210,8 +210,8 @@ beans {
         saveButton= false
         initOnPageLoad = false
         dataframeButtons = [
-//                quizzable  : [name: "quizzable", type: "link",attr:"style='color:#1976D2;'",script: """this.quizzableApp();""", "flexGridValues": ['xs0', 'sm0', 'md0', 'lg0', 'xl0']],
-                            translator  : [name: "translator", type: "link",attr:"style='color:#1976D2;margin-top:-15px;'",route: true,routeIdScript: 0, refDataframe: ref("vueTranslatorAssistantDataframe"),"flexGridValues": ['xs0', 'sm0', 'md0', 'lg0', 'xl0']],
+                            quizzable  : [name: "quizzable", type: "link",attr:"style='color:#1976D2;margin-top:-15px;'",script: """this.quizzableApp();""", "flexGridValues": ['xs0', 'sm0', 'md0', 'lg0', 'xl0']],
+                            translator : [name: "translator", type: "link",attr:"style='color:#1976D2;margin-top:-15px;'",route: true,routeIdScript: 0, refDataframe: ref("vueTranslatorAssistantDataframe"),"flexGridValues": ['xs0', 'sm0', 'md0', 'lg0', 'xl0']],
                             ecommerce  : [name: "ecommerce", type: "link",attr:"style='color:#1976D2;'",route: true,routeIdScript: 0,script: """this.ecommerceApp();""", "flexGridValues": ['xs0', 'sm0', 'md0', 'lg0', 'xl0']]]
 
         currentFrameLayout = ref("vueElintegroSubMenuDataframeLayout")
